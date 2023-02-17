789630f871ab9e563f6a3829497eb5f2727dfa06
文东柳/Dongliu Wen <gtxywdl@sina.com>
2023-02-17 10:39:58 +0800

    增加关注操作的服务，未完成

---
+++ controller/follow.go
@@ -0,0 +1,36 @@
+package controller
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+
+	url := "/douyin/relation/action/?token=&to_user_id=&action_type="
+	method := "POST"
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, nil)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	req.Header.Add("User-Agent", "Apifox/1.0.0 (https://www.apifox.cn)")
+
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(body))
+}
